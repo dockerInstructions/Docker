b8eaef403d337e89c182352ca019d575873c7779
luka.antic <luka.antic@fazi.rs>
2023-08-15 14:47:20 +0200

    First commit

---
+++ DockerProject/Program.cs
@@ -0,0 +1,10 @@
+﻿using Npgsql;
+
+string connString = "Host=db;Port=5432;Username=user;Password=pass;Database=mydatabase;";
+
+using var conn = new NpgsqlConnection(connString);
+conn.Open();
+
+using var cmd = new NpgsqlCommand("SELECT version()", conn);
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"Connected to PostgreSQL: {version}");
