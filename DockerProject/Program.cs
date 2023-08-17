0ffb5c58ae6def3f07a2481b64d272a006e3a6c1
luka.antic <luka.antic@fazi.rs>
2023-08-17 13:55:09 +0200

    return inserted row instead of pg version

--- DockerProject/Program.cs
+++ DockerProject/Program.cs
@@ -1,10 +1,37 @@
 ﻿using Npgsql;
 
-string connString = "Host=db;Port=5432;Username=user;Password=pass;Database=mydatabase;";
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connString = "Host=db;Port=5432;Username=user;Password=pass;Database=mydatabase;";
 
-using var conn = new NpgsqlConnection(connString);
-conn.Open();
+        using var conn = new NpgsqlConnection(connString);
+        conn.Open();
 
-using var cmd = new NpgsqlCommand("SELECT version()", conn);
-var version = cmd.ExecuteScalar().ToString();
-Console.WriteLine($"Connected to PostgreSQL: {version}");
+        using var createTableCmd = new NpgsqlCommand(
+            "CREATE TABLE IF NOT EXISTS Person (Id SERIAL PRIMARY KEY, Name TEXT, Age INT)",
+            conn);
+        createTableCmd.ExecuteNonQuery();
+
+        using var insertCmd = new NpgsqlCommand(
+            "INSERT INTO Person (Name, Age) VALUES (@name, @age)",
+            conn);
+        insertCmd.Parameters.AddWithValue("name", "John Doe");
+        insertCmd.Parameters.AddWithValue("age", 30);
+        insertCmd.ExecuteNonQuery();
+
+        using var selectCmd = new NpgsqlCommand(
+            "SELECT Id, Name, Age FROM Person",
+            conn);
+
+        using var reader = selectCmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int age = reader.GetInt32(2);
+            Console.WriteLine($"Person ID: {id}, Name: {name}, Age: {age}");
+        }
+    }
+}
